6898e7edbcd249c761d12755a323831e3d29f8eb
Mickael BARON <baron@ensma.fr>
2025-03-17 15:06:11 +0100

    Replace KumluzEE by OpenLiberty

--- workspace/spellwhatroyal/spellwhatroyal-server/src/main/java/fr/mickaelbaron/spellwhatroyal/server/cfg/ConfigExecution.java
+++ workspace/spellwhatroyal/spellwhatroyal-server/src/main/java/fr/mickaelbaron/spellwhatroyal/server/cfg/ConfigExecution.java
@@ -1,7 +1,10 @@
 package fr.mickaelbaron.spellwhatroyal.server.cfg;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 /**
  * @author Mickael BARON (baron.mickael@gmail.com)
@@ -9,44 +12,45 @@ import jakarta.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class ConfigExecution implements IConfigExecution {
 
-	//@Inject
-	//@ConfigProperty(name = "spellwhatroyal.init.delay", defaultValue = "0")
-	private Integer initDelay = 0;
+	@Inject
+	@ConfigProperty(name = "spellwhatroyal.init.delay", defaultValue = "0")
+	private int initDelay = 0;
 	
-	//@Inject
-	//@ConfigProperty(name = "spellwhatroyal.pregame.delay", defaultValue = "3")
-	private Integer preGameDelay = 3;
+	@Inject
+	@ConfigProperty(name = "spellwhatroyal.pregame.delay", defaultValue = "3")
+	private int preGameDelay = 3;
 	
-	//@Inject
-	//@ConfigProperty(name = "spellwhatroyal.ingame.delay", defaultValue = "10")
-	private Integer inGameDelay = 10;
+	@Inject
+	@ConfigProperty(name = "spellwhatroyal.ingame.delay", defaultValue = "10")
+	private int inGameDelay = 10;
 	
-	//@Inject
-	//@ConfigProperty(name = "spellwhatroyal.postgame.delay", defaultValue = "4")
-	private Integer postGameDelay = 4;
+	@Inject
+	@ConfigProperty(name = "spellwhatroyal.postgame.delay", defaultValue = "4")
+	private int postGameDelay = 4;
 	
 	@Override
-	public Integer getInitDelay() {
+	public int getInitDelay() {
 		return this.initDelay;
 	}
 
 	@Override
-	public Integer getPreGameDelay() {
+	public int getPreGameDelay() {
 		return this.preGameDelay;
 	}
 
 	@Override
-	public Integer getInGameDelay() {
+	public int getInGameDelay() {
 		return this.inGameDelay;
 	}
 
 	@Override
-	public Integer getPostGameDelay() {
+	public int getPostGameDelay() {
 		return this.postGameDelay;
 	}
 	
 	@PostConstruct
 	public void init() {
+		System.out.println("OULAs");
 		System.out.println(displayAllConfigExecution());
 	}
 
